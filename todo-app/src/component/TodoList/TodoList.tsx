3e407aa24b1d324b7806aeef24fe8be8a0b7de81
James <james.kirati.l@gmail.com>
2023-03-12 13:07:26 +0700

    Refactor button text in TodoList component to read "Add New Task" instead of "Please Add Todo Task".

--- todo-app/src/component/TodoList/TodoList.tsx
+++ todo-app/src/component/TodoList/TodoList.tsx
@@ -53,7 +53,7 @@ const TodoList: React.FC<Props> = ({
           className="bg-blue-500 text-white px-4 py-2 rounded text-center"
           onClick={() => setShowForm(true)}
         >
-          Please Add Todo Task
+          Add New Task
         </button>
       </div>
     </div>
