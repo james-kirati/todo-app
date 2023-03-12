4c9c78d467b677b70435e3578027f9396e5eef59
James <james.kirati.l@gmail.com>
2023-03-12 13:36:09 +0700

    Add padding to the bottom of the TodoList component's button container for better visual spacing

--- todo-app/src/component/TodoList/TodoList.tsx
+++ todo-app/src/component/TodoList/TodoList.tsx
@@ -48,7 +48,7 @@ const TodoList: React.FC<Props> = ({
         ))}
       </ul>
 
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center py-10">
         <button
           className="bg-blue-500 text-white px-4 py-2 rounded text-center"
           onClick={() => setShowForm(true)}
